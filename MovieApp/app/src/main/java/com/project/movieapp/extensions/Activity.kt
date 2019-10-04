f1652cfe11050af1be95297def60af5e301ef723
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-04 15:15:06 -0300

    added 2 kotlin extensions

---
+++ MovieApp/app/src/main/java/com/project/movieapp/extensions/Activity.kt
@@ -0,0 +1,14 @@
+package com.project.movieapp.extensions
+
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.shortToast(message: String) =
+    runOnUiThread { Toast.makeText(this, message,  Toast.LENGTH_SHORT).show() }
+
+fun AppCompatActivity.longToast(message: String) =
+    runOnUiThread { Toast.makeText(this, message, Toast.LENGTH_LONG).show() }
+
+fun AppCompatActivity.resourceToast(@StringRes message: Int) =
+    runOnUiThread { Toast.makeText(this, message, Toast.LENGTH_LONG).show() }
---
+++ MovieApp/app/src/main/java/com/project/movieapp/extensions/Glide.kt
@@ -0,0 +1,7 @@
+package com.project.movieapp.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+//todo add progress bar while loading
+fun ImageView.loadUrl(url: String?) = Glide.with(this).load(url).into(this)
