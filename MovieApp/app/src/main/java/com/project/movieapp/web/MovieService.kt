55544a91f98dcba9acb3ee361924294b4f0cfc32
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-15 20:45:43 -0300

    added url requests to MovieService

--- MovieApp/app/src/main/java/com/project/movieapp/web/MovieService.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/web/MovieService.kt
@@ -1,12 +1,27 @@
 package com.project.movieapp.web
 
+import com.project.movieapp.beans.MovieDetailsBean
+import com.project.movieapp.beans.PopularResponseBean
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
 
 
 interface MovieService {
-//todo use interceptor later to insert api key and other optinal parameters
-@GET("movie/popular?api_key=fc621c22645579c003cd78bba01d4f70&language=en-US&page=1")
-    fun getPopular(): Call<ResponseBean>
+    //todo use interceptor later to insert api key and other optinal parameters
+    @GET("movie/popular?api_key=fc621c22645579c003cd78bba01d4f70&language=en-US&page=1")
+    fun getPopular(): Call<PopularResponseBean>
+
+    @GET("movie/{movie_id}?api_key=fc621c22645579c003cd78bba01d4f70&language=en-US")
+    fun getMovieDetails(@Path("movie_id") movieId: Int): Call<MovieDetailsBean>
+//todo
+    @GET("movie/{movie_id}/reviews?api_key=fc621c22645579c003cd78bba01d4f70&language=en-US")
+    fun getListReviews(@Path("movie_id") movieId: Int): Call<MovieDetailsBean>
+//todo
+
+    @GET("review/{review_id}?api_key=fc621c22645579c003cd78bba01d4f70&language=en-US")
+    fun getReview(@Path("movie_id") movieId: Int): Call<MovieDetailsBean>
+
 
 }
