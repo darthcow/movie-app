edd7414680dd2ab4ebef915a459e63cce2dcb00a
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-15 20:42:40 -0300

    added item click listener and send movie id to next activity

--- MovieApp/app/src/main/java/com/project/movieapp/adapter/PopularListAdapter.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/adapter/PopularListAdapter.kt
@@ -1,19 +1,22 @@
 package com.project.movieapp.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.project.movieapp.MovieDetailsActivity
 import com.project.movieapp.R
 import com.project.movieapp.extensions.loadUrl
-import com.project.movieapp.web.ResultBean
+import com.project.movieapp.beans.ResultBean
 import com.project.movieapp.web.WebClient.URLConstants
 
-class PopularListAdapter(private val context: Context, val items: List<ResultBean>) :
+class PopularListAdapter(private val context: Context, private val items: List<ResultBean>) :
     RecyclerView.Adapter<Holder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder = Holder(LayoutInflater.from(context).inflate(R.layout.adapter_movie_list_item, parent, false))
 
 
@@ -27,11 +30,16 @@ class PopularListAdapter(private val context: Context, val items: List<ResultBea
 //            holder.releaseDate.text = "Release Date: $release_date"
 //            holder.averageScore.text = "Average Score: $vote_average"
             holder.moviePoster.loadUrl(URLConstants.IMAGEURL+poster_path)
+            holder.itemView.setOnClickListener { openMovieDetails(id)}
 
         }
     }
+    private fun openMovieDetails(id: Int) {
+        val intent = Intent(context, MovieDetailsActivity::class.java)
+        intent.putExtra("movieId", id)
+        context.startActivity(intent)
+    }
 }
-
 
 class Holder(itemview: View) : RecyclerView.ViewHolder(itemview) {
     //fixme refactor names later to reference adapter
