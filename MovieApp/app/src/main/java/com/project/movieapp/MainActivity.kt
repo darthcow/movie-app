3a744581c8c10c62ee6215e5fae662bd086534bd
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-16 16:03:52 -0300

    added items to ActionBar,highlight selected items and improved code readability

--- MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
@@ -2,12 +2,17 @@ package com.project.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
-import com.project.movieapp.adapter.PopularListAdapter
+import com.project.movieapp.adapter.MovieListAdapter
 import com.project.movieapp.extensions.longToast
-import com.project.movieapp.beans.PopularResponseBean
+import com.project.movieapp.beans.ListResponseBean
+import com.project.movieapp.extensions.displayMetrics
+import com.project.movieapp.extensions.shortToast
 import com.project.movieapp.web.WebClient
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,45 +21,115 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private val realmInstance : Realm  by lazy { Realm.getDefaultInstance() }
+
+    private var selectedOption: String = "popular"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPopular()
-
-        popularRecyclerView.apply {
-            //            layoutManager = LinearLayoutManager(this@MainActivity)
-            layoutManager = GridLayoutManager(this@MainActivity, 2)
+        Realm.init(this)
+        supportActionBar?.title = null
+        //todo implement programatically span count based on resolution
+        val spanCount: Int = when (this.displayMetrics().widthPixels) {
+            else -> 2
+        }
+        list_recyclerview.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, spanCount)
             itemAnimator = DefaultItemAnimator()
             setHasFixedSize(true)
+            getPopularMovies()
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.favorites_menu, menu)
+        when (selectedOption) {
+            "top" -> menu?.getItem(0)?.setIcon(R.drawable.menu_top_highlight)
+            "popular" -> menu?.getItem(1)?.setIcon(R.drawable.menu_hot_highlight)
+            "favorite" -> menu?.getItem(2)?.setIcon(R.drawable.menu_favorite_highlight)
+            "search" -> menu?.getItem(0)?.setIcon(R.drawable.menu_search_highlight)
+            else -> return true
+        }
+        return true
+    }
 
-    private fun getPopular() {
-        WebClient().movieService().getPopular().enqueue(object : Callback<PopularResponseBean> {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.top_menu_button -> getTopMovies()
+            R.id.popular_menu_button -> getPopularMovies()
+            R.id.search_menu_button -> {
+                this.shortToast("Feature yet to be implemented")
+                selectedOption = "search"
+                supportActionBar?.subtitle = "search movies"
+                invalidateOptionsMenu()
+            }
+            R.id.favorite_menu_button -> {
+                //todo implement favorites listing
+                selectedOption = "favorite"
+                supportActionBar?.subtitle = "Favorite movies"
+                list_recyclerview.adapter = null
+                invalidateOptionsMenu()
+
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun getTopMovies() {
+        WebClient().movieService().getTop().enqueue(object : Callback<ListResponseBean> {
             override fun onResponse(
-                call: Call<PopularResponseBean>,
-                popularResponse: Response<PopularResponseBean>
+                call: Call<ListResponseBean>,
+                popularResponse: Response<ListResponseBean>
             ) {
                 with(popularResponse) {
-                if (isSuccessful) {
-                    body()?.run {
-                        popularRecyclerView.adapter =
-                            PopularListAdapter(this@MainActivity, results)
+                    if (isSuccessful) {
+                        body()?.run {
+                            supportActionBar?.subtitle = "Top rated movies"
+                            supportActionBar?.displayOptions
+                            selectedOption = "top"
+                            invalidateOptionsMenu()
+                            list_recyclerview.adapter =
+                                MovieListAdapter(this@MainActivity, results)
+                        }
+                    } else {
+                        this@MainActivity.longToast(message())
                     }
-                } else {
-                    this@MainActivity.longToast(message())
                 }
             }
-        }
 
-                override fun onFailure(call: Call<PopularResponseBean>, t: Throwable) {
-//                fixme warning
-            this@MainActivity.longToast(t.message?: "unidentified error")
-        }
+            override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
+                this@MainActivity.longToast(t.message ?: "unidentified error")
+            }
+        })
+    }
 
 
-    })
-}
+    private fun getPopularMovies() {
+        WebClient().movieService().getPopular().enqueue(object : Callback<ListResponseBean> {
+            override fun onResponse(
+                call: Call<ListResponseBean>,
+                popularResponse: Response<ListResponseBean>
+            ) {
+                with(popularResponse) {
+                    if (isSuccessful) {
+                        body()?.run {
+                            supportActionBar?.subtitle = "Popular movies"
+                            selectedOption = "popular"
+                            invalidateOptionsMenu()
+                            list_recyclerview.adapter =
+                                MovieListAdapter(this@MainActivity, results)
+                        }
+                    } else {
+                        this@MainActivity.longToast(message())
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
+                this@MainActivity.longToast(t.message ?: "unidentified error")
+            }
+        })
+    }
 }
