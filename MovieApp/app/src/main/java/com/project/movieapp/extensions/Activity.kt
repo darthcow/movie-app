e45befffa616ffa79093c774eef3901695b7731a
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-17 18:37:38 -0300

    changed method that checks internet connection

--- MovieApp/app/src/main/java/com/project/movieapp/extensions/Activity.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/extensions/Activity.kt
@@ -1,10 +1,17 @@
 package com.project.movieapp.extensions
 
+import android.content.Context
 import android.util.DisplayMetrics
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import java.net.InetAddress
+import java.util.logging.Handler
+import android.content.Context.CONNECTIVITY_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.net.ConnectivityManager
+
+
 
 fun AppCompatActivity.shortToast(message: String) =
     runOnUiThread { Toast.makeText(this, message,  Toast.LENGTH_SHORT).show() }
@@ -21,14 +28,7 @@ fun AppCompatActivity.displayMetrics(): DisplayMetrics {
     windowManager.defaultDisplay.getMetrics(displayMetrics)
     return displayMetrics
 }
-
-fun AppCompatActivity.isInternetAvailable(): Boolean {
-    return try {
-        val ipAddr = InetAddress.getByName("google.com")
-        !ipAddr.equals("")
-
-    } catch (e: Exception) {
-        false
-    }
-
+ fun AppCompatActivity.isNetworkConnected(): Boolean {
+    val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager?
+    return cm!!.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
 }
