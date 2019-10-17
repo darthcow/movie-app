2ee0d87f08cff08267ebc370307fd943cf99831d
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-17 18:39:30 -0300

    removed snackbar, checks if user has internet connection

--- MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
 import com.project.movieapp.adapter.MovieListAdapter
 import com.project.movieapp.adapter.RealmMovieListAdapter
 import com.project.movieapp.beans.ListResponseBean
 import com.project.movieapp.beans.MovieDetailsBean
 import com.project.movieapp.beans.ResultBean
-import com.project.movieapp.extensions.isInternetAvailable
+import com.project.movieapp.extensions.isNetworkConnected
 import com.project.movieapp.extensions.longToast
 import com.project.movieapp.extensions.shortToast
 import com.project.movieapp.web.WebClient
@@ -29,20 +28,19 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    val snackLoading by lazy {
-        Snackbar.make(
-            list_recyclerview,
-            "Loading Movies",
-            Snackbar.LENGTH_INDEFINITE
-        )
-    }
-
     private val listResults: ArrayList<ResultBean> = arrayListOf()
 
     private var currentPage: Int = 1
     private val realmInstance: Realm by lazy { Realm.getDefaultInstance() }
 
     private var selectedOption: String = "popular"
+
+    override fun onStart() {
+        if (selectedOption == "favorite") {
+            getFavoriteMovies()
+        }
+        super.onStart()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,16 +51,20 @@ class MainActivity : AppCompatActivity() {
 //        val spanCount: Int = when (this.displayMetrics().widthPixels) {
 //            else -> 2
 //        }
+        //todo implement observable to check internet connection and enable only favorites
+
 
         list_recyclerview.apply {
             layoutManager = GridLayoutManager(this@MainActivity, 2)
             itemAnimator = DefaultItemAnimator()
             setHasFixedSize(true)
-            if (!this@MainActivity.isInternetAvailable()) {
-                selectedOption = "favorite"
-                getFavoriteMovies()
-            } else
+
+            if (this@MainActivity.isNetworkConnected())
                 getPopularMovies(currentPage)
+            else {
+                getFavoriteMovies()
+            }
+
             addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                     val recyclerLayout: GridLayoutManager =
@@ -84,10 +86,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.favorites_menu, menu)
         when (selectedOption) {
-            "top" -> menu?.getItem(0)?.setIcon(R.drawable.menu_top_highlight)
-            "popular" -> menu?.getItem(1)?.setIcon(R.drawable.menu_hot_highlight)
             "favorite" -> menu?.getItem(2)?.setIcon(R.drawable.menu_favorite_highlight)
-            "search" -> menu?.getItem(3)?.setIcon(R.drawable.menu_search_highlight)
+            "popular" -> menu?.getItem(1)?.setIcon(R.drawable.menu_hot_highlight)
+            "top" -> menu?.getItem(0)?.setIcon(R.drawable.menu_top_highlight)
+//            "search" -> menu?.getItem(3)?.setIcon(R.drawable.menu_search_highlight)
             else -> return true
         }
         return true
@@ -96,29 +98,29 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.top_menu_button -> {
-                supportActionBar?.subtitle = "Top rated movies"
-                selectedOption = "top"
-                invalidateOptionsMenu()
-                currentPage = 1
-                getTopMovies(currentPage)
+                if (this.isNetworkConnected()) {
+                    currentPage = 1
+                    getTopMovies(currentPage)
+                } else {
+                    this.longToast("To view top movies connect to the internet")
+                }
             }
             R.id.popular_menu_button -> {
-                supportActionBar?.subtitle = "Popular movies"
-                selectedOption = "popular"
-                invalidateOptionsMenu()
-                currentPage = 1
-                getPopularMovies(currentPage)
+                if (this.isNetworkConnected()) {
+                    currentPage = 1
+                    getPopularMovies(currentPage)
+                } else {
+                    this.longToast("To view popular movies connect to the internet")
+                }
             }
             R.id.search_menu_button -> {
+                //todo search button
                 this.shortToast("Feature yet to be implemented")
                 selectedOption = "search"
                 supportActionBar?.subtitle = "search movies"
                 invalidateOptionsMenu()
             }
             R.id.favorite_menu_button -> {
-                selectedOption = "favorite"
-                supportActionBar?.subtitle = "Favorite movies"
-                invalidateOptionsMenu()
                 getFavoriteMovies()
             }
             else -> return super.onOptionsItemSelected(item)
@@ -127,6 +129,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getFavoriteMovies() {
+        selectedOption = "favorite"
+        supportActionBar?.subtitle = "Favorite movies"
+        invalidateOptionsMenu()
+
         val result: RealmResults<MovieDetailsBean> =
             realmInstance.where<MovieDetailsBean>().findAll()
         if (result.size > 0)
@@ -135,7 +141,6 @@ class MainActivity : AppCompatActivity() {
             list_recyclerview.adapter = null
             shortToast("No movie favorited")
         }
-
     }
 
     override fun onDestroy() {
@@ -144,6 +149,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getTopMovies(page: Int) {
+        supportActionBar?.subtitle = "Top rated movies"
+        selectedOption = "top"
+        invalidateOptionsMenu()
         WebClient().movieService().getTop(APIKEY, page)
             .enqueue(object : Callback<ListResponseBean> {
                 override fun onResponse(
@@ -178,7 +186,6 @@ class MainActivity : AppCompatActivity() {
             }
             //api version 3 only accepts 500 pages
             page < 501 -> {
-                snackLoading.dismiss()
                 listResults.addAll(movies)
                 list_recyclerview.adapter?.notifyDataSetChanged()
             }
@@ -188,6 +195,9 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun getPopularMovies(page: Int) {
+        supportActionBar?.subtitle = "Popular movies"
+        selectedOption = "popular"
+        invalidateOptionsMenu()
         WebClient().movieService().getPopular(APIKEY, page)
             .enqueue(object : Callback<ListResponseBean> {
                 override fun onResponse(
