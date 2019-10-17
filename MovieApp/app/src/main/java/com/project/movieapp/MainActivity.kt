d046e8317c9c53ef0e47c225c5f8fae60db1ee1b
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-17 12:22:50 -0300

    Added "infinite" scrolling feature(request pagination)

--- MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.project.movieapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.project.movieapp.adapter.MovieListAdapter
 import com.project.movieapp.adapter.RealmMovieListAdapter
-import com.project.movieapp.extensions.longToast
 import com.project.movieapp.beans.ListResponseBean
 import com.project.movieapp.beans.MovieDetailsBean
 import com.project.movieapp.beans.ResultBean
-import com.project.movieapp.extensions.displayMetrics
+import com.project.movieapp.extensions.longToast
 import com.project.movieapp.extensions.shortToast
 import com.project.movieapp.web.WebClient
 import com.project.movieapp.web.WebClient.URLConstants.APIKEY
@@ -27,6 +28,17 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    val snackLoading by lazy {
+        Snackbar.make(
+            list_recyclerview,
+            "Loading Movies",
+            Snackbar.LENGTH_INDEFINITE
+        )
+    }
+
+    private val listResults: ArrayList<ResultBean> = arrayListOf()
+
+    private var currentPage: Int = 1
     private val realmInstance: Realm by lazy { Realm.getDefaultInstance() }
 
     private var selectedOption: String = "popular"
@@ -37,19 +49,51 @@ class MainActivity : AppCompatActivity() {
         Realm.init(this)
         supportActionBar?.title = null
         //todo implement programatically span count based on resolution
-        val spanCount: Int = when (this.displayMetrics().widthPixels) {
-            else -> 2
-        }
+//        val spanCount: Int = when (this.displayMetrics().widthPixels) {
+//            else -> 2
+//        }
         list_recyclerview.apply {
-            layoutManager = GridLayoutManager(this@MainActivity, spanCount)
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
             itemAnimator = DefaultItemAnimator()
             setHasFixedSize(true)
-            getPopularMovies()
+            getPopularMovies(currentPage)
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val recyclerLayout: GridLayoutManager =
+                        recyclerView.layoutManager as GridLayoutManager
+                    if (recyclerLayout.findLastVisibleItemPosition() == recyclerLayout.itemCount - 4) {
+                        if (adapter !is RealmMovieListAdapter) {
+                            when (selectedOption) {
+                                "popular" -> {
+//                                    snackLoading.show()
+                                    getPopularMovies(++currentPage)
+                                }
+                                "top" -> {
+//                                    snackLoading.show()
+                                    getTopMovies(++currentPage)
+                                }
+                                "search" -> {
+                                    //todo search movies pagination
+//                                    snackLoading.show()
+                                    getPopularMovies(++currentPage)
+                                }
+                                else -> {
+                                }
+                            }
+
+                        }
+
+
+                    }
+//                    else if (!recyclerView.canScrollVertically(-1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+//                        snackLoading.show()
+//                        getPopularMovies(--currentPage)
+//                    }
+                }
+            })
         }
     }
-
-//val result: RealmResults<MovieDetailsBean> =
-//        realmInstance.where<MovieDetailsBean>().findAll()
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -58,7 +102,7 @@ class MainActivity : AppCompatActivity() {
             "top" -> menu?.getItem(0)?.setIcon(R.drawable.menu_top_highlight)
             "popular" -> menu?.getItem(1)?.setIcon(R.drawable.menu_hot_highlight)
             "favorite" -> menu?.getItem(2)?.setIcon(R.drawable.menu_favorite_highlight)
-            "search" -> menu?.getItem(0)?.setIcon(R.drawable.menu_search_highlight)
+            "search" -> menu?.getItem(3)?.setIcon(R.drawable.menu_search_highlight)
             else -> return true
         }
         return true
@@ -66,8 +110,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.top_menu_button -> getTopMovies()
-            R.id.popular_menu_button -> getPopularMovies()
+            R.id.top_menu_button -> {
+                supportActionBar?.subtitle = "Top rated movies"
+                selectedOption = "top"
+                invalidateOptionsMenu()
+                currentPage = 1
+                getTopMovies(currentPage)
+            }
+            R.id.popular_menu_button -> {
+                supportActionBar?.subtitle = "Popular movies"
+                selectedOption = "popular"
+                invalidateOptionsMenu()
+                currentPage = 1
+                getPopularMovies(currentPage)
+            }
             R.id.search_menu_button -> {
                 this.shortToast("Feature yet to be implemented")
                 selectedOption = "search"
@@ -88,67 +144,99 @@ class MainActivity : AppCompatActivity() {
             realmInstance.where<MovieDetailsBean>().findAll()
         if (result.size > 0)
             list_recyclerview.adapter = RealmMovieListAdapter(this, result)
-        else
+        else {
             list_recyclerview.adapter = null
+            shortToast("No movie favorited")
+        }
         selectedOption = "favorite"
         supportActionBar?.subtitle = "Favorite movies"
         invalidateOptionsMenu()
 
     }
 
-    private fun getTopMovies() {
-        WebClient().movieService().getTop(APIKEY).enqueue(object : Callback<ListResponseBean> {
-            override fun onResponse(
-                call: Call<ListResponseBean>,
-                popularResponse: Response<ListResponseBean>
-            ) {
-                with(popularResponse) {
-                    if (isSuccessful) {
-                        body()?.run {
-                            supportActionBar?.subtitle = "Top rated movies"
-                            supportActionBar?.displayOptions
-                            selectedOption = "top"
-                            invalidateOptionsMenu()
-                            list_recyclerview.adapter =
-                                MovieListAdapter(this@MainActivity, results)
+    override fun onDestroy() {
+        realmInstance.close()
+        super.onDestroy()
+    }
+
+    private fun getTopMovies(page: Int) {
+        WebClient().movieService().getTop(APIKEY, page)
+            .enqueue(object : Callback<ListResponseBean> {
+                override fun onResponse(
+                    call: Call<ListResponseBean>,
+                    popularResponse: Response<ListResponseBean>
+                ) {
+                    with(popularResponse) {
+                        if (isSuccessful) {
+                            body()?.run {
+
+                                loadPages(results, page)
+                            }
+                        } else {
+                            this@MainActivity.longToast(message())
                         }
-                    } else {
-                        this@MainActivity.longToast(message())
                     }
                 }
-            }
 
-            override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
-                this@MainActivity.longToast(t.message ?: "unidentified error")
-            }
-        })
+                override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
+                    this@MainActivity.longToast(t.message ?: "unidentified error")
+                }
+            })
     }
 
 
-    private fun getPopularMovies() {
-        WebClient().movieService().getPopular(APIKEY).enqueue(object : Callback<ListResponseBean> {
-            override fun onResponse(
-                call: Call<ListResponseBean>,
-                popularResponse: Response<ListResponseBean>
-            ) {
-                with(popularResponse) {
-                    if (isSuccessful) {
-                        body()?.run {
-                            supportActionBar?.subtitle = "Popular movies"
-                            selectedOption = "popular"
-                            invalidateOptionsMenu()
-                            list_recyclerview.adapter =
-                                MovieListAdapter(this@MainActivity, results)
+    fun loadPages(movies: List<ResultBean>, page: Int) {
+
+        if (page == 1) {
+            listResults.clear()
+            listResults.addAll(movies)
+            list_recyclerview.adapter = MovieListAdapter(this@MainActivity, listResults)
+        }
+        //api version 3 only accepts 500 pages
+        else if(page<501){
+            snackLoading.dismiss()
+            listResults.addAll(movies)
+            list_recyclerview.adapter?.notifyDataSetChanged()
+//            list_recyclerview.smoothScrollToPosition(listResults.size - movies.size + 1)
+        }
+        else{
+            this.shortToast("Reached list Limit")
+        }
+    }
+
+
+    private fun getPopularMovies(page: Int) {
+        WebClient().movieService().getPopular(APIKEY, page)
+            .enqueue(object : Callback<ListResponseBean> {
+                override fun onResponse(
+                    call: Call<ListResponseBean>,
+                    popularResponse: Response<ListResponseBean>
+                ) {
+                    with(popularResponse) {
+                        if (isSuccessful) {
+                            body()?.run {
+
+                                loadPages(results, page)
+//                                if (currentPage == 1) {
+//                                    listResults = results
+//                                    list_recyclerview.adapter =
+//                                        MovieListAdapter(this@MainActivity, listResults)
+//                                } else {
+//                                    listResults.addAll(results)
+//                                    list_recyclerview.adapter?.notifyDataSetChanged()
+//
+//                                }
+
+                            }
+                        } else {
+                            this@MainActivity.longToast(message())
                         }
-                    } else {
-                        this@MainActivity.longToast(message())
                     }
                 }
-            }
 
-            override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
-                this@MainActivity.longToast(t.message ?: "unidentified error")
-            }
-        })
+                override fun onFailure(call: Call<ListResponseBean>, t: Throwable) {
+                    this@MainActivity.longToast(t.message ?: "unidentified error")
+                }
+            })
     }
 }
